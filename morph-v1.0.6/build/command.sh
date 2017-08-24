f7a19c6c47e835f280747e3b26b25281159c8803
Arthur ZWAENEPOEL <arzwa@psb.ugent.be>
2017-08-24 18:19:02 +0200

    removed 1.0.6

--- morph-v1.0.6/build/command.sh
+++
@@ -1,3 +0,0 @@
-command=(./morph ../../debug_config.yaml ../../joblist.yaml output 100)
-command=(./morph ../../debug_config.yaml ../../joblist.yaml output 100 --output-yaml)
-#command=(./morph /mnt/midas/group/biocomp/projects/morph/data/morph_results/morph_config.yaml /mnt/midas/group/biocomp/projects/morph/data/morph_results/joblist_local.yaml output 100)
